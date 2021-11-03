816545a91476e0c4598b255d3405f5a9b7a7776f
qiuxan <qiuxan@gmail.com>
2021-11-04 07:26:31 +1100

    first commit

---
+++ controllers/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xianqiu
+ * Date: 4/11/21
+ * Time: 7:21 AM
+ */
+
+namespace app\controllers;
+
+use \yii\base\Controller;
+
+
+
+class HomeController extends Controller
+{
+    public function actionIndex(){
+        echo  11232;
+    }
+
+}
